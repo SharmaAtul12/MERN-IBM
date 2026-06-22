94cb35a2f08f23b0c1eae7ffd46f92c5326600b3
SharmaAtul12 <sharmaatulas6@gmail.com>
2026-06-23 00:21:45 +0530

    Day-01 work of JS

---
+++ 04-JS/Daily/Day-1/index.js
@@ -0,0 +1,90 @@
+//! Diff bw var , let and const ----------------------------------------------------->
+
+
+// var a=15;              //var:- Can be redeclared and updated
+// //x=50;
+// console.log("\a =",a);
+
+// let b=12;            //let:- can be updated and can't be redeclared
+// b=65;
+// console.log("b =",b);
+
+// const c=5;
+// //c=9;               //const:- Can't be updated or redeclared
+// console.log("c =",c);
+
+// // var x=500;
+// // console.log(x);
+ 
+// // let b=112;
+// // console.log(b);
+
+// //! Operators ------------------------------------------------------------->
+
+// let x=4;
+// let y=4;
+
+// //checks datatype and data value
+// let ans= x===y;    
+// console.log(ans);
+
+// // It checks only data value
+// let res= x==y;    
+// console.log(res);
+
+// // not equal data value and type
+// let check= x!==y;
+// console.log(check);  
+
+// let ch= x!=y;
+// console.log(ch);
+
+
+// let add=a+c;
+// console.log("Sum is",add); 
+
+// let sub=a-c;
+// console.log("Difference is",sub);
+
+// let mul=a*c;
+// console.log("Product is",mul);
+
+// let div=a/c;
+// console.log("Quotient is",div);
+
+// let mod=a%c;
+// console.log("Modulus is",mod);
+
+//! Increment and postIncrement ---------------------------------------->
+
+// let c = 60;
+// console.log(c++,c,c++,c,c,c,c++,c++,c++,c)
+
+// console.log(--c,++c,c++,++c,++c,++c,c++,++c,++c,c--);
+
+// //! Logical Operartors => (&& , || , !)
+
+// let fullName = "Atul Sharma"
+// let firstName = "Atul"
+
+// let age = 20
+
+// if(fullName && age > 18) {
+//   console.log("Adult")
+// }
+
+// if(fullName || firstName) {
+//   console.log("Name Is Present")
+// }
+
+// if(!fullName) {
+//   console.log("Please Enter full Name")
+// }
+
+// //! Ternary Operator => (condition ? true : false)
+
+// let marks = 45
+// let result = marks >= 35 ? "Pass" : "Fail"
+// console.log(result)
+
+
